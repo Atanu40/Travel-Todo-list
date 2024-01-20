7fec995343e2baa601a5811a6c91b664c26c494b
Atanu <mightyapon98@gmail.com>
2024-01-20 13:40:33 +0600

    issue fix

--- vite-project/src/Footerstates/Footer.jsx
+++ vite-project/src/Footerstates/Footer.jsx
@@ -5,12 +5,13 @@ function Footer(props) {
   let count = 0;
   const { item } = props;
   let total = item.length;
-  item.map((items,index) =>  items.packed ? count++ : count );
+  item.map((items,index) => items.packed ? count++ : count );
   let percentage = (count/total)*100;
+  console.log(percentage);
   return (
     <div className='footer-content'>
       {
-        percentage === 100 ? <p className='footer-paragraph'>🎉 Congrats! You are ready to go!</p> :  <p className='footer-paragraph'>💼 You have {total} items on your list and you already packed {count}({percentage}%)</p>
+        percentage === 100 ? <p className='footer-paragraph'>🎉 Congrats! You are ready to go!</p> :  <p className='footer-paragraph'>💼 You have {total} items on your list and you already packed {count}({ item.length === 0 ? 0 : percentage}%)</p>
       }
      
     </div>
